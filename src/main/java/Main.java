ef4a21cef4a16719d4cb56a9e4d9024cb881bd46
GitKacperWa <kwalas@onet.pl>
2026-01-12 08:08:05 +0000

    Commit 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Wpisz liczbę cakowitą:");
+    int liczba = sc.nextInt();
+
+    if (liczba > 0){
+      System.out.println("Liczba jest dodatnia");
+    } else if (liczba < 0){
+      System.out.println("Liczba jest ujemna");
+    } else{
+      System.out.println("Liczba jest równa zero");
+    }
   }
 
   // @Test
